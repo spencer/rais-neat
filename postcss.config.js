dca3646c21a95e45839fe7baaad564d2f0e3fb96
spencer <hello@spencerturner.co.uk>
2025-10-09 10:35:31 +0100

    Update packages, add footer, fix tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+  plugins: [
+    require('postcss-import'),
+    require('@tailwindcss/postcss'),
+    require('autoprefixer'),
+  ]
+}
